d4a73bbd382cf36b184d4360c4d2eff357b3cbd3
patmuchiri <pmuchiri@gmail.com>
2024-10-14 07:51:45 +0000

    a type-annotated function sum_list which takes a list input_list of floats as argument and returns their sum as a float.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function to sum a list of floating-point numbers.
+"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns the sum of a list of floating-point numbers.
+
+    Args:
+        input_list (List[float]): A list of floats to sum.
+
+    Returns:
+        float: The sum of the floating-point numbers in the input list.
+    
+    Example:
+        >>> sum_list([3.14, 1.11, 2.22])
+        6.470000000000001
+    """
+    return sum(input_list)
