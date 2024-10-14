d32ca0a0551b77f6da841c0f569f4e09e7cfb314
patmuchiri <pmuchiri@gmail.com>
2024-10-14 07:58:39 +0000

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,6 +6,7 @@ This module contains a function to sum a list of floating-point numbers.
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """
     Returns the sum of a list of floating-point numbers.
@@ -15,7 +16,7 @@ def sum_list(input_list: List[float]) -> float:
 
     Returns:
         float: The sum of the floating-point numbers in the input list.
-    
+
     Example:
         >>> sum_list([3.14, 1.11, 2.22])
         6.470000000000001
