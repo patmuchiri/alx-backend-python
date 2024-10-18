3728f704b7f3f19a17ad7b062879573b780c24a7
patmuchiri <pmuchiri@gmail.com>
2024-10-18 06:34:02 +0000

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -17,25 +17,28 @@ def safely_get_value(
     default: Union[T, None] = None
 ) -> Union[Any, T]:
     """
-    Safely retrieves a value from a dictionary based on the key, with a fallback to a default value.
+    Safely retrieves a value from a dictionary based on the key, with
+    a fallback to a default value.
 
     Parameters:
     ----------
     dct : Mapping
         The dictionary containing keys and values. The keys can be of any type,
         and the values are of type 'T'.
-    
+
     key : Any
         The key used to retrieve the value from the dictionary.
-    
+
     default : Union[T, None], optional
-        The default value to return if the key does not exist in the dictionary.
+        The default value to return if the key does not exist in
+        the dictionary.
         Defaults to None.
 
     Returns:
     -------
     Union[Any, T]
-        The value from the dictionary corresponding to the given key, or the default
+        The value from the dictionary corresponding to the given key,
+        or the default
         value if the key is not found.
     """
     if key in dct:
